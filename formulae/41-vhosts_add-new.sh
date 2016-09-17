3705d0c1faff5dc411ee98bcbacd397b14f8f3f6
Jonas Friedmann <j@frd.mn>
2016-09-18 00:10:27 +0200

    Make sure to check if indeed a directory

--- formulae/41-vhosts_add-new.sh
+++ formulae/41-vhosts_add-new.sh
@@ -49,38 +49,41 @@ fi
 if [[ ! -z $(find /var/www/html/ -maxdepth 1 -type d ! -path /var/www/html/) ]]; then
     # For each custom virtual host
     for directory in /var/www/html/*; do
-        # Store hostname in variable and substitute dots with dashes for MySQL
-        virtual_hostname=$(basename "${directory}")
-        virtual_db_hostname=${virtual_hostname/./_}
-        virtual_db_hostname=${virtual_db_hostname:0:16}
+        # Check if indeed directory
+        if [[ -d "${directory}" ]]; then
+            # Store hostname in variable and substitute dots with dashes for MySQL
+            virtual_hostname=$(basename "${directory}")
+            virtual_db_hostname=${virtual_hostname/./_}
+            virtual_db_hostname=${virtual_db_hostname:0:16}
 
-        # Check if vhost was already created, if not create
-        if [[ ! -f "/opt/servant/vhosts/${virtual_hostname}" ]]; then
-            # Create necessary folders
-            sudo mkdir -p ${directory}/{htdocs,logs,backups}
+            # Check if vhost was already created, if not create
+            if [[ ! -f "/opt/servant/vhosts/${virtual_hostname}" ]]; then
+                # Create necessary folders
+                sudo mkdir -p ${directory}/{htdocs,logs,backups}
 
-            # write configuration file
-            sudo bash -c "cat > /etc/apache2/sites-available/${virtual_hostname}.conf" <<< "$(print_apache_vhost)"
+                # write configuration file
+                sudo bash -c "cat > /etc/apache2/sites-available/${virtual_hostname}.conf" <<< "$(print_apache_vhost)"
 
-            # Enable config
-            sudo a2ensite ${virtual_hostname}.conf | prefix "${virtual_hostname}][Apache"
+                # Enable config
+                sudo a2ensite ${virtual_hostname}.conf | prefix "${virtual_hostname}][Apache"
 
-            # Create MySQL database and user
-            MYSQL_PWD=${args_root_password} mysql -u root -e """
-            CREATE DATABASE IF NOT EXISTS \`${virtual_db_hostname}\` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
-            GRANT ALL ON \`${virtual_db_hostname}\`.* TO \`${virtual_db_hostname}\`@'localhost' IDENTIFIED BY '${virtual_db_hostname}';
-            """
+                # Create MySQL database and user
+                MYSQL_PWD=${args_root_password} mysql -u root -e """
+                CREATE DATABASE IF NOT EXISTS \`${virtual_db_hostname}\` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
+                GRANT ALL ON \`${virtual_db_hostname}\`.* TO \`${virtual_db_hostname}\`@'localhost' IDENTIFIED BY '${virtual_db_hostname}';
+                """
 
-            # Store lockfile / password for the database
-            printf "${virtual_db_hostname}" > /opt/servant/mysql/${virtual_hostname}
+                # Store lockfile / password for the database
+                printf "${virtual_db_hostname}" > /opt/servant/mysql/${virtual_hostname}
 
-            # Make sure to restart Apache at the end of the script
-            touch /opt/servant/apache.restart
+                # Make sure to restart Apache at the end of the script
+                touch /opt/servant/apache.restart
 
-            # Create lockfile
-            touch /opt/servant/vhosts/${virtual_hostname}
+                # Create lockfile
+                touch /opt/servant/vhosts/${virtual_hostname}
 
-            echo "Created user and database \"${virtual_db_hostname}\"" | prefix "${virtual_hostname}][MySQL"
+                echo "Created user and database \"${virtual_db_hostname}\"" | prefix "${virtual_hostname}][MySQL"
+            fi
         fi
     done
 fi
